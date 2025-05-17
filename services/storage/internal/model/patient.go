5a96382b22b00a0f5f2c1da6c3c36260e4c7917a
Sergey <109383267+SerGGe43@users.noreply.github.com>
2025-05-17 16:54:10 +0300

    feat: добавлен CRUD пациента

---
+++ services/storage/internal/model/patient.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type Patient struct {
+	ID          UserID    `db:"user_id"`
+	FirstName   string    `db:"first_name"`
+	SecondName  string    `db:"second_name"`
+	Surname     string    `db:"surname"`
+	Email       string    `db:"email"`
+	BirthDate   time.Time `db:"birth_date"`
+	PhoneNumber string    `db:"phone_number"`
+	Gender      string    `db:"gender"`
+}
